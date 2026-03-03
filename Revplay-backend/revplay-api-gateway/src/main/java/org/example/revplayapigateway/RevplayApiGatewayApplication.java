d0c5f8b7fdf72db52ec835bfbd32663dc1fee41e
Hemanth <hemanth@gmail.com>
2026-03-03 22:46:42 +0530

    Initial commit: Workspace setup, Eureka Server, and API Gateway

---
+++ Revplay-backend/revplay-api-gateway/src/main/java/org/example/revplayapigateway/RevplayApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.revplayapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevplayApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevplayApiGatewayApplication.class, args);
+    }
+
+}
---
+++ Revplay-backend/revplay-api-gateway/src/test/java/org/example/revplayapigateway/RevplayApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.revplayapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevplayApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
